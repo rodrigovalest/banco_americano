a8aabc2558086a490dc754471076c3702f8a850f
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-06-25 16:31:30 -0300

    [add] setting up ms-costumer project

---
+++ ms-costumer/src/test/java/com/rodrigovalest/ms_costumer/MsCostumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigovalest.ms_costumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCostumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
