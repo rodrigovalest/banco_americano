68632734af02e3ba182e8ed85ddc45786bea03f5
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-06-25 17:40:41 -0300

    [add] setting up ms-payment project

---
+++ ms-payment/src/main/java/com/rodrigovalest/ms_payment/MsPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigovalest.ms_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPaymentApplication.class, args);
+	}
+
+}
