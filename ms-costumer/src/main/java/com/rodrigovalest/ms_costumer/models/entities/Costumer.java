997ce9b9e15b3d68a8533cb09f1ecf39a74e1f38
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-06-26 16:53:50 -0300

    [add] costumer entity

---
+++ ms-costumer/src/main/java/com/rodrigovalest/ms_costumer/models/entities/Costumer.java
@@ -0,0 +1,44 @@
+package com.rodrigovalest.ms_costumer.models.entities;
+
+import com.rodrigovalest.ms_costumer.models.enums.GenderEnum;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_costumers")
+public class Costumer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "cpf", nullable = false)
+    private String cpf;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", nullable = false)
+    private GenderEnum gender;
+
+    @Column(name = "birthdate", nullable = false)
+    private LocalDate birthdate;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "points")
+    private Long points = 0L;
+
+    @Column(name = "url_photo")
+    private String urlPhoto;
+}
---
+++ ms-costumer/src/main/java/com/rodrigovalest/ms_costumer/models/enums/GenderEnum.java
@@ -0,0 +1,9 @@
+package com.rodrigovalest.ms_costumer.models.enums;
+
+public enum GenderEnum {
+    MALE,
+    FEMALE,
+    NON_BINARY,
+    OTHER,
+    PREFER_NOT_TO_SAY
+}
