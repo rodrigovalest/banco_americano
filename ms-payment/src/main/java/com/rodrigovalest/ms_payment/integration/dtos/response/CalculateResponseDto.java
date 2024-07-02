2dd1c11814c58e369615ee50e6ca6e3b979146ec
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-07-02 20:45:08 -0300

    [add] creating create payment, find by id, find by customer id endpoints with their respective unit tests

---
+++ ms-payment/src/main/java/com/rodrigovalest/ms_payment/integration/dtos/response/CalculateResponseDto.java
@@ -0,0 +1,11 @@
+package com.rodrigovalest.ms_payment.integration.dtos.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CalculateResponseDto {
+    private Long points;
+}
