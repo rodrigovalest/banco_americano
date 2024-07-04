fa4a88ccca31a04b39e7bf2edd2e2e3015b23eee
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2024-07-04 19:44:00 -0300

    [add] documenting endpoints of ms-payment using swagger

--- ms-payment/src/main/java/com/rodrigovalest/ms_payment/web/controllers/PaymentController.java
+++ ms-payment/src/main/java/com/rodrigovalest/ms_payment/web/controllers/PaymentController.java
@@ -5,6 +5,12 @@ import com.rodrigovalest.ms_payment.services.PaymentService;
 import com.rodrigovalest.ms_payment.web.dtos.mapper.PaymentMapper;
 import com.rodrigovalest.ms_payment.web.dtos.request.CreatePaymentRequestDto;
 import com.rodrigovalest.ms_payment.web.dtos.response.PaymentResponseDto;
+import com.rodrigovalest.ms_payment.web.exceptions.RestErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +22,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Tag(name = "Payment", description = "Feature to create, get by id and get by customer payments")
 @RestController
 @RequestMapping("/v1/payments")
 public class PaymentController {
@@ -23,6 +30,13 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @Operation(
+            description = "creates a new payment",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "CREATED"),
+                    @ApiResponse(responseCode = "422", description = "UNPROCESSABLE ENTITY",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = RestErrorMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<Void> create(@Valid @RequestBody CreatePaymentRequestDto createPaymentRequestDto) {
         Payment payment = this.paymentService.create(PaymentMapper.toEntity(createPaymentRequestDto));
@@ -30,12 +44,28 @@ public class PaymentController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(
+            description = "find a payment by their id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK",
+                            content = @Content(mediaType = " application/json;charset=UTF-8")),
+                    @ApiResponse(responseCode = "404", description = "NOT FOUND",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = RestErrorMessage.class)))
+            })
     @GetMapping("/{paymentId}")
     public ResponseEntity<PaymentResponseDto> findById(@PathVariable("paymentId") UUID paymentId) {
         Payment payment = this.paymentService.findById(paymentId);
         return ResponseEntity.ok(PaymentMapper.toDto(payment));
     }
 
+    @Operation(
+            description = "find payments by customer",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK",
+                            content = @Content(mediaType = " application/json;charset=UTF-8")),
+                    @ApiResponse(responseCode = "404", description = "NOT FOUND",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = RestErrorMessage.class)))
+            })
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<PaymentResponseDto>> findByCustomerId(@PathVariable("userId") Long userId) {
         List<Payment> paymentList = this.paymentService.findByCustomerId(userId);
